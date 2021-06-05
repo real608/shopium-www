d6de0eedc711226f3f45c4b653ba559b3a22eeba
BaguetteBling <59348201+real608@users.noreply.github.com>
2021-06-05 19:07:42 -0400

    Create filteringscriptmaybe.js

---
+++ resources/filteringscriptmaybe.js
@@ -0,0 +1,10 @@
+function replace(element, from, to) {
+    if (element.childNodes.length) {
+        element.childNodes.forEach(child => replace(child, from, to));
+    } else {
+        const cont = element.textContent;
+        if (cont) element.textContent = cont.replace(from, to);
+    }
+};
+
+replace(document.body, new RegExp("hello", "g"), "hi");
